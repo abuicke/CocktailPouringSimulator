f69558d5ed09f1df3b55c8673c4504b184da0b18
abuicke <mosesjayjayjay@gmail.com>
2018-12-30 01:04:42 +0000

    weight sensor working

--- src/main/java/lt/soe/autocock/weightsensor/Hx711WeightSensor.java
+++ src/main/java/lt/soe/autocock/weightsensor/Hx711WeightSensor.java
@@ -28,6 +28,9 @@ public class Hx711WeightSensor {
             SAUCER_COCKTAIL_GLASS
     };
 
+    private static CocktailGlass cocktailGlass;
+    private static double currentSensorWeight = 0.0D;
+
     public static void main(String[] args) {
         System.out.println("weight sensor initialised");
         try (ZContext context = new ZContext()) {
@@ -36,17 +39,22 @@ public class Hx711WeightSensor {
                 replySocket.bind("tcp://*:5555");
                 System.out.println("reply socket bound to TCP port 5555, waiting for request from server...");
                 byte[] reply = replySocket.recv(0);
-                if (reply[0] == 1) {
-                    System.out.println("received request from sever to start running weight sensor simulation");
+
+                boolean isFirstIngredient = reply[0] == 0;
+                int ingredientQuantity = ByteBuffer.wrap(reply).getInt(1);
+
+                if (isFirstIngredient) {
+                    currentSensorWeight = 0.0D;
                 }
-                System.out.println("closes reply socket, request has been received");
+
+                System.out.println("isFirstIngredient = " + isFirstIngredient);
+                System.out.println("ingredientQuantity = " + ingredientQuantity + "mls");
+
                 replySocket.send(new byte[]{0});
                 replySocket.close();
 
-                System.out.println("opening publisher socket to begin sending weight sensor readings");
                 ZMQ.Socket publisherSocket = context.createSocket(ZMQ.PUB);
                 publisherSocket.bind("tcp://*:5555");
-                System.out.println("publisher socket bound to TCP port 5555");
 
                 //  Ensure subscriber connection has time to complete
                 try {
@@ -55,55 +63,42 @@ public class Hx711WeightSensor {
                     throw new IllegalStateException(ie);
                 }
 
-                // Set initial weight to 0.0
-                double currentSensorWeight = 0.0D;
-                CocktailGlass cocktailGlass = getRandomCocktailGlass();
-
                 try {
-                    // Send initial weight of 0.0
-                    send(publisherSocket, currentSensorWeight);
-                    System.out.println("sent initial weight reading 0.0");
-
-                    System.out.println("placing empty glass on the weight sensor");
-                    // Wait between 1 and 5 seconds to place the glass on the sensor.
-                    Thread.sleep(1000 + (1000 * new Random().nextInt(5)));
-                    System.out.println("empty glass has been placed on the weight sensor");
-
                     // Put the glass on the weight sensor.
-                    currentSensorWeight += cocktailGlass.weightGrams;
-                    send(publisherSocket, currentSensorWeight);
-                    System.out.println("sent simulated weight reading " +
-                            currentSensorWeight + " of empty glass placed on sensor");
+                    if (isFirstIngredient) {
+                        // Wait between 1 and 5 seconds to place the glass on the sensor.
+                        Thread.sleep(1000 + (1000 * new Random().nextInt(5)));
+                        cocktailGlass = getRandomCocktailGlass();
+                        currentSensorWeight += cocktailGlass.weightGrams;
+                        send(publisherSocket, currentSensorWeight);
+                        System.out.println("empty glass has been placed on the " +
+                                "weight sensor, weight of glass = " + currentSensorWeight + "g");
+                    }
                 } catch (InterruptedException e) {
                     throw new IllegalStateException(e);
                 }
 
-                // Fill the glass to 95% capacity.
-                double volumeMillilitres = ((double) cocktailGlass.volumeMillilitres) * 0.95D;
-                // Get the weight in grams of the liquid when it fills the glass
-                double gramsOfLiquid = volumeMillilitres * AVERAGE_GRAMS_PER_MILLILITRE;
-                double finalWeight = currentSensorWeight + gramsOfLiquid;
-
-                System.out.println("filling glass...");
-                while (currentSensorWeight < finalWeight) {
+                System.out.println("filling glass with " + ingredientQuantity + "mls of ingredient");
+                double amountOfIngredientPouredWeight = 0;
+                double filledIngredientWeight = ingredientQuantity * AVERAGE_GRAMS_PER_MILLILITRE;
+                while (amountOfIngredientPouredWeight < filledIngredientWeight) {
                     // Get flow rate per tenth of a second.
-                    currentSensorWeight += (FLOW_RATE_GRAMS_PER_SECOND * 0.1);
-//                    System.out.println("sending updated weight detected by " +
-//                            "sensor as glass is being filled, " + currentSensorWeight);
-                    send(publisherSocket, currentSensorWeight);
+                    amountOfIngredientPouredWeight += FLOW_RATE_GRAMS_PER_SECOND * 0.1;
+                    System.out.println("poured " +
+                            (amountOfIngredientPouredWeight / AVERAGE_GRAMS_PER_MILLILITRE) +
+                            "mls of " + ingredientQuantity + "mls");
+                    send(publisherSocket, currentSensorWeight + amountOfIngredientPouredWeight);
                     try {
                         Thread.sleep(100);
                     } catch (InterruptedException e) {
                         throw new IllegalStateException(e);
                     }
                 }
+
+                currentSensorWeight += filledIngredientWeight;
                 send(publisherSocket, -1);
                 System.out.println("glass has been filled");
-                System.out.println("closing socket to server...");
                 publisherSocket.close();
-                System.out.println("socket has been closed");
-                System.out.println("weight sensor simulator shutting down...");
-//            System.exit(0);
             }
         }
     }
