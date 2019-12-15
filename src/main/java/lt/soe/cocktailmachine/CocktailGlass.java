2fe159265e59c9bdc9d51456c0f1c4e702e0dcbf
abuicke <mosesjayjayjay@gmail.com>
2019-12-15 15:51:29 +0000

    added documentation to CocktialGlass class
    added documentation to CocktialGlass class

--- src/main/java/lt/soe/cocktailmachine/CocktailGlass.java
+++ src/main/java/lt/soe/cocktailmachine/CocktailGlass.java
@@ -1,5 +1,11 @@
 package lt.soe.cocktailmachine;
 
+/**
+ * A cocktail glass.
+ * <p>
+ * A cocktail glass is defined by the volume of
+ * liquid it can hold and the weight of the glass.
+ * */
 @SuppressWarnings("WeakerAccess")
 public class CocktailGlass {
 
